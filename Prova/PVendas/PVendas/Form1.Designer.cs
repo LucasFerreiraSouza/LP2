4d6d4de570786959651f95014f0673801a4ff9c8
Lucas Ferreira de Souza <99302284+LucasFerreiraSouza@users.noreply.github.com>
2022-05-20 22:10:48 -0300

    Add files via upload

---
+++ Prova/PVendas/PVendas/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace PVendas
+{
+    partial class frmPVendas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstbxDados = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(281, 200);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lstbxDados
+            // 
+            this.lstbxDados.FormattingEnabled = true;
+            this.lstbxDados.Location = new System.Drawing.Point(428, 83);
+            this.lstbxDados.Name = "lstbxDados";
+            this.lstbxDados.Size = new System.Drawing.Size(293, 225);
+            this.lstbxDados.TabIndex = 2;
+            // 
+            // frmPVendas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbxDados);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "frmPVendas";
+            this.Text = "PVendas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstbxDados;
+    }
+}
+
---
+++ Prova/PVendas/PVendas/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PVendas
+{
+    public partial class frmPVendas : Form
+    {
+        public frmPVendas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string auxiliar1;
+            double[,] dados = new double[2, 4];
+            double[] totalMes = new double[4];
+            double total;
+            double totalm;
+            double totalFinal;
+
+            total = 0;
+            totalm = 0;
+            totalFinal = 0;
+            totalMes[0] = 0;
+
+            for (var l = 0; l < 2; l++)
+            {
+                for (var c = 0; c < 4; c++)//Faz primeiro, (0,0) (0,1) (0,2)
+                {
+                    auxiliar1 = Interaction.InputBox("Mês " + (l + 1) + " semana " + (c + 1), "Entrada de Dados");
+                    if (!double.TryParse(auxiliar1, out dados[l, c]))
+                    {
+                        MessageBox.Show("Valor inválido");
+                        c = c - 1;
+                    }
+                    else
+                    {
+                        if (dados[l, c] < 0 || c == -1) // Se der alguma coisa errada
+                        {
+                            c = 0;
+                            totalMes[l] = dados[l, c];// Variável acumuladora
+                            MessageBox.Show("Digite um número maior que zero");
+                            c = c - 1;
+
+                        }
+                        else
+                        {
+                            c = c + 0;//não muda
+                            totalMes[l] = dados[l, c];// Variável acumuladora
+
+                            lstbxDados.Items.Add("Total do mês: " + (l + 1) + " Semana: " + (c + 1) + " " + totalMes[l].ToString("C2"));
+
+                            if (l == 0)
+                            {
+                                total = total + totalMes[l];
+                            }
+                            total = 0;
+
+                            total = total + totalMes[l];
+                            totalm = totalm+total;
+
+
+                            totalFinal = totalFinal + total;
+                        }
+                    }
+                }
+                lstbxDados.Items.Add("Total mês: " + totalm.ToString("C2"));
+            }
+            lstbxDados.Items.Add("Total Geral: " + totalFinal.ToString("C2"));
+
+            auxiliar1 = "";
+        }
+    }
+}
